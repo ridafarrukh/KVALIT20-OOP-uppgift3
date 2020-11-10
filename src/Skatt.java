bc35dc2d2299ecc415f001f99ac9252bc91d6992
ridafarrukh <ridafarrukh92@gmail.com>
2020-11-10 12:05:05 +0100

    first commit

---
+++ src/Skatt.java
@@ -0,0 +1,10 @@
+/**
+ * created by Rida Warsi
+ * Date : 2020-11-10
+ * Time : 12:01
+ * Project : KVALIT20-OOP-Inlämningsuppgift3
+ * Copyright : MIT
+ */
+
+public class Skatt {
+}
---
+++ src/SkattDemo.java
@@ -0,0 +1,14 @@
+/**
+ * created by Rida Warsi
+ * Date : 2020-11-10
+ * Time : 12:01
+ * Project : KVALIT20-OOP-Inlämningsuppgift3
+ * Copyright : MIT
+ */
+
+public class SkattDemo {
+    public static void main(String[] args) {
+        
+    }
+}
+
