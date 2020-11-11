60085020aea484c26c6e387c2528afbddcae2c92
ridafarrukh <ridafarrukh92@gmail.com>
2020-11-11 10:53:21 +0100

    create file Skatt

--- src/Skatt.java
+++ src/Skatt.java
@@ -7,4 +7,95 @@
  */
 
 public class Skatt {
+
+    //Instansvariabler
+    String namn;
+    int åik;    //årsinkomst
+
+
+    // flera viktiga Instansvariabler
+    String inkomstskatt;
+    int grundAvdrag;
+    int Besk;     // beskattningsbarförvärvsinkomst
+    int ösg;      // övre skiktgränsen
+    int nsg;     // nedre skiktgränsen
+    int ööp;     // över ösg skatt
+    int möns;    // mellan ösg till nsg skatt
+    int iks;    // inkomstskatt
+    int es;    // efterskatt
+
+
+    // skapa instansmetoder
+
+    /**
+     * En instansmetod som returnerar namnet
+     *
+     * @return "Namn"
+     */
+
+    public String getName() {
+
+        return this.namn;
+    }
+
+    /**
+     * En instansmetod som beräknar Besk (Beskattningsbar förvärvsinkomst)
+     *
+     * @return Besk
+     */
+
+    public int getBesk() {
+
+        return Besk = åik - grundAvdrag;
+    }
+
+
+    /**
+     *
+     * En instansmetod som beräknar Inkomstskatt i olika nivåer
+     *
+     * @return Inkomstskatt
+     */
+
+    public String getInkomstskatt() {
+        int skatt = this.getBesk();
+
+        String category = "";
+
+        if (Besk <= nsg)
+            // behövs inte betala statlig inkomstskatt
+            category = " 0 kr";
+
+        else if (Besk > nsg && Besk <= ösg) {
+            //behöv betala 20%  statlig inkomstskatt.
+            möns = Besk - nsg;
+            iks = möns / 100 * 20;
+            category = iks + " kr ";
+        }
+        else if (Besk > ösg){
+            // behovs betala 25% statlig inkomstskatt
+            ööp =(Besk - ösg)/100 * 5;
+            möns =(ösg - nsg)/100 * 20;
+            iks = ööp + möns;
+            category = iks + " kr ";
+        }
+
+        return category;
+
+
+    }
+
+    /**
+     * En instansmetod som beräknar ÅrsInkomst ( efterskatt)
+     *
+     * @return Inkomst
+     */
+
+    public int getInkomst(){
+        es = Besk - iks;
+        return this.es;
+
+
+    }
 }
+
--- src/SkattDemo.java
+++ src/SkattDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * created by Rida Warsi
  * Date : 2020-11-10
@@ -8,7 +10,33 @@
 
 public class SkattDemo {
     public static void main(String[] args) {
-        
+
+       //skapa en scanner som läser från kommandofönstret.
+
+        Scanner ds = new Scanner(System.in);
+        Scanner minScanner = new Scanner(System.in);
+
+        System.out.println("Ange ditt namn");
+        String name = minScanner.nextLine();
+
+        System.out.println("Ange ditt Årsinkomst");
+        String inkomst = minScanner.nextLine();
+        int aik =  Integer.parseInt(inkomst);
+
+
+        Skatt skattObj  = new Skatt();
+        skattObj.namn = name;
+        skattObj.åik = aik;
+        skattObj.grundAvdrag = 13_400;
+        skattObj.nsg  = 455_000;
+        skattObj.ösg = 662_300;
+
+
+        // skriv ut objekt
+        System.out.println("Namn:" + skattObj.getName() );
+        System.out.println("Besk:" + skattObj.getBesk() + " kr ");
+        System.out.println("InkomstSkatt:" + skattObj.getInkomstskatt() );
+        System.out.println("Årsinkomst:" + skattObj.getInkomst() + " kr ");
     }
 }
 
